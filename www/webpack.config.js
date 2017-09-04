0f2c73e674460ca71a0d68bb50ed28e6a75d68a1
Moritz Gunz <moritz.gunz@gmail.com>
2017-09-04 17:36:36 +0200

    Revert ":memo: Switch TypeDoc to modules mode"
    This reverts commit b09ace410657317bcc5b73d180be52f6499b2108.

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
             excludePrivate: true,
             ignoreCompilerErrors: true,
             name: "Cordova Spotify OAuth Plugin",
-            mode: 'module',
+            mode: 'file',
             readme: 'none',
             target: 'ES6'
         }, './spotify-oauth.ts')
